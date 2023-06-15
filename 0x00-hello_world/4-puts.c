8d4394143710456c8cae09921f78e2e2f4e05ca6
Aminebr23 <amine.bourachdi@etu.fstm.ac.ma>
2023-06-15 12:25:40 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program with puts function
+ * Return: 0 (Succes)
+ */
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}	
